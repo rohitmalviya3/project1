31512e60357c95c0d50e1dd25165108b328aff57
ROHIT <rohit.malviya@spsu.ac.in>
2022-05-26 14:36:50 +0530

    First commit

---
+++ variables.java
@@ -0,0 +1,18 @@
+public class variables {
+    public static void main(String args[]) {
+        int age = 20-6;
+        int marks = 100-6;
+         age = marks;
+         System.out.println(age);
+         System.out.println(marks);
+         boolean party = true;
+         long mobile_number = 999999999999L;
+         float fraction = 1.3f;
+
+
+
+
+        
+
+    }
+}
